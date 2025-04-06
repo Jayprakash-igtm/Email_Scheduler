b4117fda2abecadb9f408bc127a484a9bfe311be
Jayprakash-igtm <128076498+Jayprakash-igtm@users.noreply.github.com>
2025-04-06 18:41:32 +0530

    Update index.js
    updated cors for acceptance from vercel

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://email-scheduler-pi.vercel.app/',
   credentials: true,
 }));
 app.use(express.json({ limit: '10mb' }));
